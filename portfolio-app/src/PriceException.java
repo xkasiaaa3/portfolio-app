8cbcde3b9849b66a9433db513bafd25f119305e8
Daniel Ojeda Chen <daniel.oje.che@gmail.com>
2025-08-11 14:22:14 +0000

    Added equals and price exceptions stock

---
+++ portfolio-app/src/PriceException.java
@@ -0,0 +1,5 @@
+public class PriceException extends Exception {
+    public PriceException() {
+        super("Price entered is not possible");
+    }
+}
--- portfolio-app/src/Stock.java
+++ portfolio-app/src/Stock.java
@@ -1,4 +1,5 @@
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Stock {
 
@@ -24,7 +25,11 @@ public class Stock {
         return pricePerShare;
     }
     public void updatePricePerShare(BigDecimal newPrice) {
-        this.pricePerShare = newPrice;
+        if (newPrice.compareTo(new BigDecimal("0")) > 0) {
+            this.pricePerShare = newPrice;
+        } else {
+            System.out.println("Invalid input, try again.");
+        }
     }
     public int getQuantity() {
         return quantity;
@@ -43,4 +48,17 @@ public class Stock {
                 "Stock Price per share: " + pricePerShare + "\n" +
                 "Stock Quantity: " + quantity + "\n";
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Stock stock = (Stock) o;
+        return stockId == stock.stockId && Objects.equals(stockName, stock.stockName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(stockId, stockName, quantity, pricePerShare);
+    }
 }
+
