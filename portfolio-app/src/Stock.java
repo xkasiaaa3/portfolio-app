e81920e2c82eea19b6019cf9bed3df0b1f63131f
Daniel Ojeda Chen <daniel.oje.che@gmail.com>
2025-08-11 12:53:28 +0000

    Skeleton stock class

--- portfolio-app/src/Stock.java
+++ portfolio-app/src/Stock.java
@@ -1,7 +1,33 @@
 import java.math.BigDecimal;
 
 public class Stock {
+
     int stockId;
+    String stockName;
     int quantity;
     BigDecimal pricePerShare;
+
+    public BigDecimal getPricePerShare() {
+        return pricePerShare;
+    }
+    public void updatePricePerShare(BigDecimal newPrice) {
+        this.pricePerShare = newPrice;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void buyStock(int buyQuantity) {
+        this.quantity += buyQuantity;
+    }
+    public void sellStock(int sellQuantity) {
+        this.quantity -= sellQuantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock ID: " + stockId + "\n" +
+                "Stock Name: " + stockName + "\n" +
+                "Stock Price per share: " + pricePerShare + "\n" +
+                "Stock Quantity: " + quantity + "\n";
+    }
 }
